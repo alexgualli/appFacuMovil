e57cf96ad2649c008028287bb0d3a3e9e1a75c58
Alex Gualli <alex.gualli.ga@gmail.com>
2019-05-22 18:49:06 -0500

    Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'events', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
  // { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  //{ path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
+  { path: 'login', loadChildren: './pages/login/login.module#LoginPageModule' },
   //{ path: 'user', loadChildren: './pages/user/user.module#UserPageModule' },
   { path: 'events', loadChildren: './pages/events/events.module#EventsPageModule' },
   { path: 'slider', loadChildren: './pages/slider/slider.module#SliderPageModule' },
