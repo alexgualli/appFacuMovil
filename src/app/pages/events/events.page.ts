6f592a6423be5aa1a1c343d680f3245033cda32a
Joaquin Porras <erickjpv@hotmail.es>
2019-05-29 00:32:50 -0500

    Implementación de Slider en Events

--- src/app/pages/events/events.page.ts
+++ src/app/pages/events/events.page.ts
@@ -3,10 +3,9 @@ import { EventService } from 'src/app/services/serviceEvent/event.service';
 
 import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner/ngx';
 
-import {Dialogs} from '@ionic-native/dialogs/ngx'
+import { Dialogs } from '@ionic-native/dialogs/ngx'
 
-import { ModalController, Platform } from '@ionic/angular';
-import {ModalImagePage} from '../modal-image/modal-image.page'; 
+import { ModalController, Platform, MenuController } from '@ionic/angular';
 
 
 
@@ -17,17 +16,25 @@ import {ModalImagePage} from '../modal-image/modal-image.page';
 })
 export class EventsPage implements OnInit {
 
-  qrScan:any;
-  eventos: any[];
-  constructor(public platform:Platform,public dialog:Dialogs,private qrScanner: QRScanner,public modalController: ModalController,private eventService: EventService) { 
-    this.platform.backButton.subscribeWithPriority(0,()=>{
-      document.getElementsByTagName("body")[0].style.opacity="1";
+  qrScan: any;
+  events: any[];
+  constructor(
+    public platform: Platform,
+    public dialog: Dialogs,
+    private qrScanner: QRScanner,
+    public modalController: ModalController,
+    private eventService: EventService,
+    private menu: MenuController
+  ) {
+
+  this.platform.backButton.subscribeWithPriority(0, () => {
+      document.getElementsByTagName("body")[0].style.opacity = "1";
       this.qrScan.unsubscribe();
     })
   }
 
   ngOnInit() {
-    this.eventos = [];
+    this.events = [];
     this.getAllEvents();
   }
 
@@ -35,40 +42,39 @@ export class EventsPage implements OnInit {
     this.eventService.getAllEvents()
       .subscribe((res: any) => {
         for (var i = 0; i < res.length; i++) {
-          var evento = res[i];
-          console.log(evento)
-          this.eventos.push(evento);
+          var event = res[i];
+          console.log(event)
+          this.events.push(event);
         }
       })
   }
 
-  qr:string='No hay nada';
+  qr: string = 'No hay nada';
 
-  scan(){
-    this.qrScanner.prepare().then((status:QRScannerStatus)=>{
-      if(status.authorized){
+  scan() {
+    this.qrScanner.prepare().then((status: QRScannerStatus) => {
+      if (status.authorized) {
         this.qrScanner.show();
-          document.getElementsByTagName("body")[0].style.opacity="0";
-          this.qrScan=this.qrScanner.scan().subscribe((textFound)=>{
-          document.getElementsByTagName("body")[0].style.opacity="1";
+        document.getElementsByTagName("body")[0].style.opacity = "0";
+        this.qrScan = this.qrScanner.scan().subscribe((textFound) => {
+          document.getElementsByTagName("body")[0].style.opacity = "1";
           this.qrScan.unsubscribe();
           this.dialog.alert(textFound);
-          this.qr=textFound;
-          },(err)=>{
-            this.dialog.alert(JSON.stringify(err));
-          })
+          this.qr = textFound;
+        }, (err) => {
+          this.dialog.alert(JSON.stringify(err));
+        })
       }
-      else{
-        if(status.denied){
+      else
+      {
+        if (status.denied) {
           this.dialog.alert("textFound");
-
         }
-
       }
     })
   }
 
-
-  
-  
+  openFirst() {
+    this.menu.open('first');
+  }
 }
