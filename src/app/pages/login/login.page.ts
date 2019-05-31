cfaedcf15038507d8c19a630ed5ec4a6dc208839
Alex Gualli <alex.gualli.ga@gmail.com>
2019-05-31 12:17:57 -0500

    Funcion Login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from 'src/app/services/serviceUser/user-service.service';
+import { AlertServiceService } from 'src/app/services/serviceAlert/alert-service.service';
+import { NavController, ModalController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor( private modalController: ModalController,
+    private authService: UserServiceService,
+    private navCtrl: NavController,
+    private alertService: AlertServiceService) { }
 
   ngOnInit() {
+  }
+
+  dismissLogin() {
+    this.modalController.dismiss();
+  }
+  login(form: NgForm) {
+    console.log(form.value.email +'  '+ form.value.password);
+    this.authService.login(form.value.email, form.value.password).subscribe(
+      data => {
+        this.alertService.presentToast("Welcome");
+      },
+      error => {
+        console.log(error);
+      },
+      () => {
+        this.dismissLogin();
+        this.navCtrl.navigateRoot('/events');
+      }
+    );
   }
 
 }
